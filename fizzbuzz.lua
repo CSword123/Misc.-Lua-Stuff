f41637ff89b29a62735f5ddbd5ba3fe4cbfd5995
Hector Esparza <h.esparza80@gmail.com>
2018-09-14 18:56:16 -0400

    Updated rule for FizzBuzz
    Now prints "FizzBuzz" too.

--- fizzbuzz.lua
+++ fizzbuzz.lua
@@ -1,13 +1,15 @@
---Create a program that increments and everytime it encounters a multiple of 3, print "fizz" while a multiple of 5 displays "Buzz" instead.
+--Create a program that increments and everytime it encounters a multiple of 3, print "Fizz" while a multiple of 5 displays "Buzz" instead.
+--When a number that is both a multiple of 3 and 5 is encountered, print "FizzBuzz".
 
 local counter = 0;
 local f = "Fizz"; 
 local b = "Buzz";
+local fb = "FizzBuzz";
 local multCounter = 1;
 local hit1 = 3;
 local hit2 = 5;
 
-function fizzbuzz() 
+function fizzBuzz() 
 
   while (counter < 100) do
     counter = counter + 1;
@@ -18,10 +20,12 @@ function fizzbuzz()
     elseif counter == hit2 then
       print(b);
       hit2 = hit2 * (multCounter + 1);
+    elseif counter % 15 == 0 then
+      print(fb);
     else
       print(counter);
     end
   end
 end
 
-fizzbuzz();
+fizzBuzz();
